0c28bc4198e03b784861be10e644671379ee1a67
Christian Stein <sormuras@gmail.com>
2022-02-03 22:46:49 +0100

    Add UPL license header

--- test/Test.java
+++ test/Test.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates.
+ *
+ * This source code is licensed under the UPL license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
 import java.util.ArrayList;
 import java.util.stream.Stream;
 
