ea6e8d89bdefcf98dbd7ba5442fe354a4f50a6d9
Christian Stein <sormuras@gmail.com>
2022-10-19 19:36:58 +0200

    Revert to use deprecated `set-output` for the time being

--- src/Download.java
+++ src/Download.java
@@ -230,6 +230,10 @@ public class Download {
   static class GitHub {
     /** Sets an action's output parameter. */
     static void setOutput(String name, Object value) {
+      if (Boolean.getBoolean(/*-D*/ "eprecatedSetOutput")) {
+        System.out.printf("::set-output name=%s::%s%n", name, value);
+        return;
+      }
       if (name.isBlank() || value.toString().isBlank()) { // implicit null checks included
         throw new IllegalArgumentException("name or value are blank: " + name + "=" + value);
       }
@@ -244,6 +248,7 @@ public class Download {
         if (lines.size() != 1) {
           throw new UnsupportedOperationException("Multiline strings are no supported");
         }
+        debug("Write output %s to %s".formatted(lines, file));
         Files.write(file, lines, UTF_8, CREATE, APPEND, WRITE);
       } catch (IOException exception) {
         throw new UncheckedIOException(exception);
