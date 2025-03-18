44aa12602ef06f130f0f66ca5b14139a373a4c68
Christian Stein <sormuras@gmail.com>
2025-03-18 15:00:24 +0100

    Java 24 / JDK 24: General Availability
    Closes #95

--- test/Test.java
+++ test/Test.java
@@ -29,6 +29,7 @@ public class Test {
   static void checkAllOracleJDKs() {
     System.out.println();
     System.out.println("// oracle.com - latest");
+    checkOracleJDK("24", "latest");
     checkOracleJDK("23", "latest");
     checkOracleJDK("21", "latest");
 
